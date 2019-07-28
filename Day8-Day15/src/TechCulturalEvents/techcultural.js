e372b83046a2268f60ecf1c8baddd53592847ee1
Gautami12 <49782910+Gautami12@users.noreply.github.com>
2019-07-29 02:04:48 +0530

    Add files via upload

---
+++ Day8-Day15/src/TechCulturalEvents/techcultural.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './techcultural.css';
+/*const sections = [
+    {
+  url: "https://s3.ap-south-1.amazonaws.com/townscript-production/images/f2b6e37c-7a7e-422a-84b9-0c219eb29911.jpeg",
+  title: "SIES GST Tatva Moksh"
+    },
+    {
+        url: "https://scontent-dus1-1.cdninstagram.com/vp/35f1f3630fcfab08db586d899ecd143c/5D9F4288/t51.2885-15/e35/38757440_1808487922592087_5241511125203288064_n.jpg?_nc_ht=scontent-dus1-1.cdninstagram.com&se=5&ig_cache_key=MTg0NTg3OTU3OTM1NDc5NDc5NA%3D%3D.2",
+  title: "SIES GST Tatva Moksh"
+  
+    },
+    {
+        url: "http://tedxsiesgst.com/assets/tedxsiesgst_black_og.jpg",
+  title: "TEDxSIESGST"
+  
+    },
+    {
+    url: "https://cdn1.studentcompetitions.com/dragonfly_images/W1siZiIsIjIwMTcvMDYvMDQvMTcvMjAvMzYvZmZjOWVlNDgtYTZjNS00N2RiLWI3NDItZWE1OTc3NDAzZGNjLzIxMDIyLmpwZyJdLFsicCIsInRodW1iIiwiMjgweDI0MCJdXQ",
+  title: "CodeChef SIES GST Campus"
+    }
+   
+  ]
+  */
+function TechCultural(){
+   
+return (
+    <div className = "parent"> 
+  <div className = "parentImage"><img title = "SIES GST Tatva Moksh" src = {"https://s3.ap-south-1.amazonaws.com/townscript-production/images/f2b6e37c-7a7e-422a-84b9-0c219eb29911.jpeg"} className = "imagetag"></img>
+   </div>
+
+  <div className = "parentImage"><img title= "Technical Seminar" alt="alternatee" src = {"https://scontent-dus1-1.cdninstagram.com/vp/35f1f3630fcfab08db586d899ecd143c/5D9F4288/t51.2885-15/e35/38757440_1808487922592087_5241511125203288064_n.jpg?_nc_ht=scontent-dus1-1.cdninstagram.com&se=5&ig_cache_key=MTg0NTg3OTU3OTM1NDc5NDc5NA%3D%3D.2"} className = "imagetag"></img> </div>
+  
+  <div className = "parentImage"><img title= "TEDxSIESGST" src = {"http://tedxsiesgst.com/assets/tedxsiesgst_black_og.jpg"} className = "imagetag"></img> </div>
+  <div className = "parentImage"><img title= "CODECHEF" src = {"https://cdn1.studentcompetitions.com/dragonfly_images/W1siZiIsIjIwMTcvMDYvMDQvMTcvMjAvMzYvZmZjOWVlNDgtYTZjNS00N2RiLWI3NDItZWE1OTc3NDAzZGNjLzIxMDIyLmpwZyJdLFsicCIsInRodW1iIiwiMjgweDI0MCJdXQ"} className = "imagetag"></img> </div>
+  <div className = "parentImage"><img title= "FLASHMOB" src = {"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmz1lmMsExYHmXlNfclWLZLWOI0q2NH5a6J2vAGYXr0y2W9uSS"} className = "imagetag"></img> </div>
+  <div className = "parentImage"><img title = "TML"src = {"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh5FE1p1lteoHYKmKHaqA-yYgB_jFnp48sxSPUtChMZy7AS_c-xA"} className = "imagetag"></img> </div>
+  <div className = "parentImage"><img title = "EDC" src = {"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2ho1Io8uVvghkk4yQ2IbD1pOnjFv5YbJRLehdgjr7wXOoXeFG"} className = "imagetag"></img> </div>
+  <div className = "parentImage"><img title = "DREAM RUN" src = {"https://storage.googleapis.com/ehimages/2018/2/24/img_b9fc53567cbb4d318bc26126d539e8be_1519474916316_original.jpg"} className = "imagetag"></img> </div>
+  <div className = "parentImage"><img title = "FAREWELL" src = {"https://i.ytimg.com/vi/rzzgof4jaG8/maxresdefault.jpg"} className = "imagetag"></img> </div>
+    </div>
+
+)
+    
+}
+export default TechCultural;
