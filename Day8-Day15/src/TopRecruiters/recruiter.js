b6bcb75ed957914407a67a5b7006461fb3ddfc02
Gautami12 <49782910+Gautami12@users.noreply.github.com>
2019-07-29 02:09:24 +0530

    Add files via upload

---
+++ Day8-Day15/src/TopRecruiters/recruiter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './toprecruiter.css';
+function Recruiter (){
+return (
+<div className = "parent-job">
+    <div><img src = "http://www.siesgst.edu.in/assets/company/Top/ibm.png"></img></div>
+    <div><img src = "http://www.siesgst.edu.in/assets/company/Top/jacobs.png"></img></div>
+    <div><img src = "http://www.siesgst.edu.in/assets/company/Top/cadbury.png"></img></div>
+    <div><img src = "http://www.siesgst.edu.in/assets/company/Top/capgemini.png"></img></div>
+    <div><img src = "http://www.siesgst.edu.in/assets/company/Top/dabur.png"></img></div>
+    <div><img src = "http://www.siesgst.edu.in/assets/company/Top/oracle.png"></img></div>
+    <div><img src = "http://www.siesgst.edu.in/assets/company/Top/tcs.png"></img></div>
+    <div><img src = "http://www.siesgst.edu.in/assets/company/Top/techmahindra.png"></img></div>
+    <div><img src = "http://www.siesgst.edu.in/assets/company/Top/l&t.png"></img></div>
+    <div><img src = "http://www.siesgst.edu.in/assets/company/Top/zte.png"></img></div>
+
+</div>
+
+)
+
+}
+export default Recruiter;
