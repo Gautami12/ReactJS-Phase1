e3cc47c6db9a2ac0224ececffc016e0f68333755
Gautami12 <49782910+Gautami12@users.noreply.github.com>
2019-07-29 02:05:48 +0530

    Add files via upload

---
+++ Day8-Day15/src/Announcements/announcements.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './announcements.css';
+
+function Announcement ()   {
+    return (
+        <div className="parent-nav1">
+<h1 className="an">Announcements</h1>
+<div><p>Merit List after the Counseling Round - I</p></div>
+<div><p>INSTITUTE LEVEL CONSOLIDATED MERIT LIST 2019-20</p></div>
+<div><p>INSTITUTE LEVEL CONSOLIDATED MERIT LIST 2019-20</p></div>
+<div><p>Guidance Session for First Year Engineering Admissions</p></div>
+        </div>
+
+    )
+
+
+
+}
+export default Announcement;
